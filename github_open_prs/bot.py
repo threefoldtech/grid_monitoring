2a5e3a67c21d4b85d340c46ae550d5d39f9ee6ef
Mostafa Hassan <mhost39@gmail.com>
2021-04-15 13:15:14 +0200

    add github open PRs

---
+++ github_open_prs/bot.py
@@ -0,0 +1,26 @@
+# first install telegram package: pip3 install python-telegram-bot
+import telegram
+import os
+from .github_prs import prs_sdk, prs_jsng, prs_charts
+
+#token that can be generated talking with @BotFather on telegram
+my_token = 'bot_token'
+
+def send(msg, chat_id, token=my_token):
+    """
+    Send a mensage to a telegram user specified on chatId
+    chat_id must be a number!
+    """
+    bot = telegram.Bot(token=token)
+    bot.sendMessage(chat_id=chat_id, text=msg)
+
+if __name__ == "__main__":
+    msgs = [prs_jsng, prs_sdk, prs_charts]
+    github_prs_token = os.environ.get("PRs_TOKEN")
+    chat_id = os.environ.get("CHAT_ID_OPEN_PRs")
+
+    for m in msgs:
+        separator = '\n'
+        if len(m) > 1:
+            ms = separator.join(m)
+            send(msg=ms, chat_id=chat_id, token=my_token)
---
+++ github_open_prs/github_prs.py
@@ -0,0 +1,26 @@
+from github import Github
+import os
+
+import ipdb
+
+github_token = os.environ.get("GITHUB_TOKEN")
+
+def get_prs_title(prs, repo_name):
+    prs_title= [f">>>>>> {repo_name} PRs >>>>>>\n"]
+    for pr in prs:
+        prs_title.append(f"{pr.title}: {pr.html_url} \n")
+    return prs_title
+
+g = Github(login_or_token=github_token, per_page=100)
+r = g.get_repo("threefoldtech/js-sdk")
+
+all_prs_sdk = r.get_pulls('open')
+prs_sdk = get_prs_title(all_prs_sdk, "SDK")
+
+r = g.get_repo("threefoldtech/js-ng")
+all_prs_jsng = r.get_pulls('open')
+prs_jsng = get_prs_title(all_prs_jsng, "jsng")
+
+r = g.get_repo("threefoldtech/vdc-solutions-charts")
+all_prs_charts = r.get_pulls('open')
+prs_charts = get_prs_title(all_prs_charts, "vdc-solutions-charts")
